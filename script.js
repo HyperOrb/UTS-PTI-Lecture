4a40111c765db6c20ffdbc658664e3c7ae4d75d8
Ryann Chandiari <ryann.chandiari@gmail.com>
2024-03-18 23:22:37 +0700

    main

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-$(document).ready(function(){
-    $('#myModal').modal('show');
+var typed = new Typed('.auto-type', {
+  strings: ["You", "Ourselves", "😭😭😔😔", "Love", "Passion", "Happiness", "Yourself", "We", "Mankind"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+$(document).ready(function () {
+  $('#myModal').modal('show');
 });
 
 function saveUserData() {
-    var name = document.getElementById("modalnameInput").value;
-
-    document.getElementById("userName").textContent = name;
-
-    $('#myModal').modal('hide');
-    document.getElementById("greetings").style.display = "block";
+  var name = document.getElementById("modalnameInput").value;
+  document.getElementById("userName").textContent = name;
+  $('#myModal').modal('hide');
+  $('#greetings').fadeIn().css("display", "flex").addClass("animated bounceInLeft");
 }
 
 function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-
-    var timeString = padZero(hours) + ":" + padZero(minutes) + ":" + padZero(seconds);
-
-    document.getElementById('clock').textContent = timeString;
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var timeString = padZero(hours) + ":" + padZero(minutes) + ":" + padZero(seconds);
+  document.getElementById('clock').textContent = timeString;
 }
 
 function padZero(num) {
-    return (num < 10 ? '0' : '') + num;
+  return (num < 10 ? '0' : '') + num;
 }
 
 setInterval(updateClock, 1000);
-
 updateClock();
 
-
 var data = [
-    { nim: '00000092718', name: 'Ryann Chandiari', alamat: 'Makassar Pride' },
-    { nim: 'Rine123', name: '123Sugar', alamat: 'Redoxon' }
+  { nim: '00000092718', name: 'Ryann Chandiari', alamat: 'Makassar Pride' },
+  { nim: 'Rine123', name: '123Sugar', alamat: 'Redoxon' }
 ];
 
 function populateTable() {
-    var tableBody = $('#dataTableBody');
-    tableBody.empty(); 
+  var tableBody = $('#dataTableBody');
+  tableBody.empty();
 
-    $.each(data, function(index, item) {
-        var row = $('<tr>');
-        row.append($('<td>').text(item.nim));
-        row.append($('<td>').text(item.name));
-        row.append($('<td>').text(item.alamat));
-        row.append($('<td>').html('<button class="btn btn-primary btn-sm edit-btn">Edit</button> <button class="btn btn-danger btn-sm delete-btn">Delete</button>'));
+  var entriesPerPage = parseInt($('#entriesPerPage').val());
+  var currentPage = 1;
 
-        tableBody.append(row);
-    });
+  var startIndex = (currentPage - 1) * entriesPerPage;
+  var endIndex = startIndex + entriesPerPage;
+  if (endIndex > data.length) {
+    endIndex = data.length;
+  }
+
+  for (var i = startIndex; i < endIndex; i++) {
+    var item = data[i];
+    var row = $('<tr>');
+    row.append($('<td>').text(item.nim));
+    row.append($('<td>').text(item.name));
+    row.append($('<td>').text(item.alamat));
+    row.append($('<td>').html('<button class="btn btn-primary btn-sm edit-btn">Edit</button> <button class="btn btn-danger btn-sm delete-btn">Delete</button>'));
+    tableBody.append(row);
+  }
+
+  var entryCountMessage = "Showing " + (startIndex + 1) + " to " + endIndex + " of " + data.length + " entries.";
+  $('#tableInfo').text(entryCountMessage);
 }
 
-$('#addDataForm').submit(function(event) {
-    event.preventDefault();
-    var nim = $('#nimInput').val();
-    var name = $('#nameInput').val();
-    var alamat = $('#alamatInput').val();
-    data.push({ nim: nim, name: name, alamat: alamat });
-    populateTable();
-    $('#nimInput, #nameInput, #alamatInput').val('');
+$('#addDataForm').submit(function (event) {
+  event.preventDefault();
+  var nim = $('#nimInput').val();
+  var name = $('#nameInput').val();
+  var alamat = $('#alamatInput').val();
+  data.push({ nim: nim, name: name, alamat: alamat });
+  populateTable();
+  $('#nimInput, #nameInput, #alamatInput').val('');
 });
 
-$('th').click(function() {
-    var column = $(this).index();
-    var sortOrder = $(this).data('sort-order') || 'asc';
-    data.sort(function(a, b) {
-        var aValue = a[Object.keys(a)[column]];
-        var bValue = b[Object.keys(b)[column]];
-        if (sortOrder === 'asc') {
-            return aValue.localeCompare(bValue);
-        } else {
-            return bValue.localeCompare(aValue);
-        }
-    });
-    $(this).data('sort-order', sortOrder === 'asc' ? 'desc' : 'asc');
-    populateTable();
+$('#entriesPerPage').change(function () {
+  populateTable();
 });
 
 populateTable();
